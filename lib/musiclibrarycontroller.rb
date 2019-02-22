80367fadfb081ab7eda187453057c8ca0bd12794
blackwell <blackwellsmithiv@gmail.com>
2019-02-22 15:36:18 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -80,7 +80,7 @@ end
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
    if Song.all.length + 1 >= input && input > 0
-     song = Song.all.sort_by! { |songA| songA.name}
+     song = Song.all.sort_by! { |songA| songA.name}[input - 1]
      binding.pry
   end
 end
