f35b772718ec66fa6528aa76184f70e3cc845fa7
blackwell <blackwellsmithiv@gmail.com>
2019-02-22 13:44:04 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -46,7 +46,14 @@ end
   list = Genre.all.sort_by! do |genre| 
     genre.name
    end
-   
+   def list_artists
+  list = Artist.all.sort_by! do |artist| 
+    artist.name
+   end
+   list.each.with_index(1) do |artist, i|
+     puts "#{i}. #{artist.name}"
+   end
+  end
    def list_artists
   list = Artist.all.sort_by! do |artist| 
     artist.name
