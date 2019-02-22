ce76d06ccacb4c67cc182ab08d07ed0c00ff59c3
blackwell <blackwellsmithiv@gmail.com>
2019-02-22 15:27:57 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -79,7 +79,7 @@ end
   def play_song
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
-   if Song.all.length + 1 >= input
+   if Song.all.length + 1 >= input && input > 0
    binding.pry
     end
   end
