60c317640193c71710c55e0ffbd85ecdc6cc669b
blackwell <blackwellsmithiv@gmail.com>
2019-02-22 16:14:48 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,10 +16,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-   
+    
     i = 0
  loop do
-   input = gets.strip
+   
   i += 1
   if i == 4 || input == "exit"
     break       
